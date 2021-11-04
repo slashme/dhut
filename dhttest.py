c0f5a1850bd99949b26169477e616ccf7c87fdea
David Richfield <davidrichfield@gmail.com>
2021-11-04 22:44:51 +0100

    Changed to DHT22 sensor
    Now I'm using a DHT22 sensor I don't need code to support a separate
    temperature probe. Added information to website about which sensor is
    sending data. Currently hardcoded.
    
    Added #!/usr/bin/python3 to top to allow direct execution.

--- dhttest.py
+++ dhttest.py
@@ -1,45 +1,20 @@
+#!/usr/bin/python3
 import Adafruit_DHT
 import time
 import datetime
 import pytz
 import urllib.request
-import glob
  
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-def read_temp():
-    lines = read_temp_raw()
-    while len(lines) < 2 or lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
-
-DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
- 
+DHT_sensor = 0
+
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    temperature = read_temp()
     if humidity is not None and temperature is not None:
         a=datetime.datetime.now().astimezone(pytz.utc)
         try:
-            blah=urllib.request.urlopen('https://dhut.herokuapp.com/add?when={}-{}-{}%20{}%3a{}%3a{}.{}%2B00:00&temp={}&rh={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity))
+            blah=urllib.request.urlopen('https://dhut.herokuapp.com/add?when={}-{}-{}%20{}%3a{}%3a{}.{}%2B00:00&temp={}&rh={}&sensor={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity, DHT_sensor))
         except urllib.error.URLError as error:
             print("Error in connection: ", error)
-        #print('http://localhost:5000/add?when={}-{}-{}%20{}%3a{}%3a{}.{}&temp={}&rh={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity))
-        print(temperature,humidity)
-    else:
-        print("Sensor failure. Check wiring.")
     time.sleep(60)
