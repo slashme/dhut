872ddbde4edbb4e4caf093a3505f9ed69353c551
David Richfield <davidrichfield@gmail.com>
2021-11-06 12:59:16 +0100

    Move sensor list to config file

--- raspberryscripts/dhttest.py
+++ raspberryscripts/dhttest.py
@@ -3,29 +3,19 @@ import Adafruit_DHT
 import time
 import datetime
 import pytz
+import json
 import urllib.request
  
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
-DHT_sensor = 1
-
-DHT_SENSOR2 = Adafruit_DHT.DHT11
-DHT_PIN2 = 18
-DHT_sensor2 = 3
+with open('sensorlist.json', 'r') as f:
+    sensorlist = json.load(f)
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        a=datetime.datetime.now().astimezone(pytz.utc)
-        try:
-            blah=urllib.request.urlopen('https://dhut.herokuapp.com/add?when={}-{}-{}%20{}%3a{}%3a{}.{}%2B00:00&temp={}&rh={}&sensor={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity, DHT_sensor))
-        except urllib.error.URLError as error:
-            print("Error in connection: ", error)
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR2, DHT_PIN2)
-    if humidity is not None and temperature is not None:
-        a=datetime.datetime.now().astimezone(pytz.utc)
-        try:
-            blah=urllib.request.urlopen('https://dhut.herokuapp.com/add?when={}-{}-{}%20{}%3a{}%3a{}.{}%2B00:00&temp={}&rh={}&sensor={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity, DHT_sensor2))
-        except urllib.error.URLError as error:
-            print("Error in connection: ", error)
+    for sensor in sensorlist:
+        humidity, temperature = Adafruit_DHT.read_retry(eval(sensor['model']), sensor['pin'])
+        if humidity is not None and temperature is not None:
+            a=datetime.datetime.now().astimezone(pytz.utc)
+            try:
+                blah=urllib.request.urlopen('https://dhut.herokuapp.com/add?when={}-{}-{}%20{}%3a{}%3a{}.{}%2B00:00&temp={}&rh={}&sensor={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity, sensor['sensor_id']))
+            except urllib.error.URLError as error:
+                print("Error in connection: ", error)
     time.sleep(60)
