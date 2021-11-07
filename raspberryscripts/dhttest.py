4c3e8c0c213b380ee2986a069285ca6d098f597b
David Richfield <davidrichfield@gmail.com>
2021-11-07 12:54:40 +0100

    Run each sensor in its own thread
    Add sensor read delay to sensor list file.
    For each sensor in the list, start a separate thread.

--- raspberryscripts/dhttest.py
+++ raspberryscripts/dhttest.py
@@ -5,17 +5,27 @@ import datetime
 import pytz
 import json
 import urllib.request
+import threading
  
 with open('/home/david/sensorlist.json', 'r') as f:
     sensorlist = json.load(f)
 
-while True:
-    for sensor in sensorlist:
-        humidity, temperature = Adafruit_DHT.read_retry(eval(sensor['model']), sensor['pin'])
+def sensorloop(model, pin, sensor_id, delay):
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(eval(model), pin)
         if humidity is not None and temperature is not None:
             a=datetime.datetime.now().astimezone(pytz.utc)
             try:
                 blah=urllib.request.urlopen('https://dhut.herokuapp.com/add?when={}-{}-{}%20{}%3a{}%3a{}.{}%2B00:00&temp={}&rh={}&sensor={}'.format(a.year, a.month, a.day, a.hour, a.minute, a.second, a.microsecond, temperature, humidity, sensor['sensor_id']))
             except urllib.error.URLError as error:
                 print("Error in connection: ", error)
-    time.sleep(60)
+        time.sleep(delay)
+
+for sensor in sensorlist:
+    x = threading.Thread(target=sensorloop,
+                         args = (sensor['model'],
+                                 sensor['pin'],
+                                 sensor['sensor_id'],
+                                 sensor['delay'])
+                         )
+    x.start()
